cebc26c5611b013adcdbd07f1eae853ebe37797f
Matheus Mota Tonini <138072866+motaxyz@users.noreply.github.com>
2024-03-26 20:33:53 -0300

    Update

--- aula_1/array.js
+++ aula_1/array.js
@@ -1,17 +1,34 @@
-let aluno1 = 'Nei Junio';
-let aluno2 = 'Gabriel';
-let aluno3 = 'Matheus';
-let aluno4 = 'Weslley';
+let alunos = [
+    'Gabriel',
+    'Matheus',
+    'Nei',
+    'Weslley',
+];
 
 console.log('Lista de Alunos: ');
-console.log('');
-console.log('- ' + aluno1);
-console.log('- ' + aluno2);
-console.log('- ' + aluno3);
-console.log('- ' + aluno4);
-console.log('');
-console.log('');
-console.log('');
-console.log('');
+
+for (let n = 0; n < alunos.length; n++) {
+    console.log('- ' + alunos[n]);
+}
+
 console.log('');
 console.log('Fim do Programa');
+
+
+
+
+
+
+// let aluno1 = 'Nei Junio';
+// let aluno2 = 'Gabriel';
+// let aluno3 = 'Matheus';
+// let aluno4 = 'Weslley';
+
+// console.log('Lista de Alunos: ');
+// console.log('');
+// console.log('- ' + aluno1);
+// console.log('- ' + aluno2);
+// console.log('- ' + aluno3);
+// console.log('- ' + aluno4);
+// console.log('');
+// console.log('Fim do Programa');
